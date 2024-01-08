1f34388ab50de4483607d6e84db744d4134d8dd1
Mohammad Nawas <mohammed8.nawas@gmail.com>
2024-01-08 16:43:25 +0200

    Custom Cell

--- ColorTasks-ColorList/ColorsViewController.swift
+++ ColorTasks-ColorList/ColorsViewController.swift
@@ -8,36 +8,28 @@
 import UIKit
 
 class ColorsViewController: UIViewController {
-    
-    var tableView: UITableView!
-    var descriptionView: UIView!
-    var descriptionHeaderLabel: UILabel!
-    var descriptionLabel: UILabel!
-    
-    var portraitConstraints = [NSLayoutConstraint]()
-    var landscapeConstraints = [NSLayoutConstraint]()
 
+    @IBOutlet var colorsTableView: UITableView!
+    
     var selectedColor = colorsList.first {
         didSet {
-            descriptionView.backgroundColor = selectedColor?.color
-            descriptionLabel.text = selectedColor?.description
+
         }
     }
     
     private struct Constants {
         static let CELL_HEIGHT = CGFloat(integerLiteral: 47)
         static let CELL_IDENTIFIER = "colorCellId"
+        static let CELL_NIB_NAME = "ColorTableViewCell"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
-        portraitConstraints = getPortraitModeConstraints()
-        landscapeConstraints = getLandscapeModeConstraints()
+        
         showAppropriateView()
     }
     
-    // The issue is in deactivating the constraints,
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         showAppropriateView()
@@ -45,9 +37,9 @@ class ColorsViewController: UIViewController {
     
     func showAppropriateView(){
         if UIDevice.current.orientation.isPortrait {
-            portraitView()
+           
         } else {
-            landscapeView()
+            
         }
     }
     
@@ -56,87 +48,19 @@ class ColorsViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.isNavigationBarHidden = false
         title = "Colors"
-        NSLayoutConstraint.deactivate(landscapeConstraints)
-        NSLayoutConstraint.activate(portraitConstraints)
+       
     }
     
     func landscapeView(){
         navigationController?.isNavigationBarHidden = true
-        NSLayoutConstraint.deactivate(portraitConstraints)
-        NSLayoutConstraint.activate(landscapeConstraints)
+
     }
     
     func configureViews(){
-        tableView = UITableView()
-        tableView.register(ColorCellTableViewCell.self, forCellReuseIdentifier: Constants.CELL_IDENTIFIER)
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = Constants.CELL_HEIGHT
-        
-        descriptionView = UIView()
-        descriptionView.backgroundColor = selectedColor?.color
-        view.addSubview(descriptionView)
-        descriptionView.translatesAutoresizingMaskIntoConstraints = false
-        
-        descriptionHeaderLabel = UILabel()
-        descriptionHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionHeaderLabel.text = "Description"
-        descriptionHeaderLabel.textColor = .white
-        descriptionHeaderLabel.font = .systemFont(ofSize: 24, weight: .bold)
-        descriptionView.addSubview(descriptionHeaderLabel)
-        
-        descriptionLabel = UILabel()
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.numberOfLines = 0
-        descriptionLabel.text = selectedColor?.description
-        descriptionLabel.textColor = .white
-        descriptionLabel.font = .systemFont(ofSize: 16)
-        descriptionView.addSubview(descriptionLabel)
-
-    }
-    
-    func getPortraitModeConstraints() -> [NSLayoutConstraint]{
-        return [
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
-            
-            descriptionView.topAnchor.constraint(equalTo: tableView.bottomAnchor),
-            descriptionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            descriptionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            descriptionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
-            descriptionHeaderLabel.leadingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.leadingAnchor),
-            descriptionHeaderLabel.topAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.topAnchor),
-
-            descriptionLabel.leadingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.leadingAnchor),
-            descriptionLabel.trailingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.trailingAnchor),
-            descriptionLabel.topAnchor.constraint(equalTo: descriptionHeaderLabel.bottomAnchor, constant: 15),
-        ]
-    }
-    
-    func getLandscapeModeConstraints() -> [NSLayoutConstraint] {
-        return [
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-
-            descriptionView.topAnchor.constraint(equalTo: view.topAnchor),
-            descriptionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            descriptionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            descriptionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3),
-
-            descriptionHeaderLabel.leadingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.leadingAnchor),
-            descriptionHeaderLabel.topAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.topAnchor),
-
-            descriptionLabel.leadingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.leadingAnchor),
-            descriptionLabel.trailingAnchor.constraint(equalTo: descriptionView.layoutMarginsGuide.trailingAnchor),
-            descriptionLabel.topAnchor.constraint(equalTo: descriptionHeaderLabel.bottomAnchor, constant: 15),
-        ]
+        colorsTableView.delegate = self
+        colorsTableView.dataSource = self
+        let cellNib = UINib(nibName: Constants.CELL_NIB_NAME, bundle: nil) // main bundle
+        colorsTableView.register(cellNib, forCellReuseIdentifier: Constants.CELL_IDENTIFIER)
     }
 }
 
@@ -146,9 +70,9 @@ extension ColorsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CELL_IDENTIFIER)
-            as! ColorCellTableViewCell
-        cell.set(colorInfo: colorsList[indexPath.row])
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CELL_IDENTIFIER, for: indexPath) as! ColorTableViewCell
+        cell.colorLabel.text = colorsList[indexPath.row].name
+        cell.backgroundColor = colorsList[indexPath.row].value
         return cell
     }
     
